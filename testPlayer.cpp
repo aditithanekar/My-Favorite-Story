187273a501e975a8bccfcca28d64d3691cf0207e
aditithanekar <aditipthanekar@gmail.com>
2023-05-18 19:08:35 -0700

    added tests for Player

--- testPlayer.cpp
+++ testPlayer.cpp
@@ -1,2 +1,10 @@
 //#include gtest
 //only rose will make cmake and then later merge 
+TEST(PlayerConstructor, DefaultConstructor)
+{
+    EXPECT_NO_THROW(Player("Vanilla"));
+}
+TEST(PlayerConstructor, ParametrizedConstructor)
+{
+    EXPECT_NO_THROW(Player());
+}
