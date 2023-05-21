5e06e1aa85eff5edca147beaec245be9d72c8eb5
xwRongx <46xxrwongxx64@gmail.com>
2023-05-20 20:55:56 -0700

    Scene.cpp + cmakelists

---
+++ headers/Scene.h
@@ -0,0 +1,22 @@
+#include <string>
+
+#ifndef __SCENE_H__
+#define __SCENE_H__
+
+using namespace std;
+
+class Scene {
+private:
+    string graphics;
+    int textIndex; //current index in text array
+    int totalTexts; //total number of texts in array
+    string text[30];
+public:
+    Scene(string fileName);
+    ~Scene();
+
+    //returns current item in array of strings, increment textIndex
+    string getText();
+};
+
+#endif
