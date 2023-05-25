1457abec381f5494d4b1c17d8072aa9c101475f6
aditithanekar <aditipthanekar@gmail.com>
2023-05-24 22:05:31 -0700

    added SceneNode struct, updated return for getText,and displayScene()

--- src/Genre.cpp
+++ src/Genre.cpp
@@ -13,3 +13,15 @@ Genre::Genre(string genreName){
 string Genre::getGenreName(){
     return genreName;
 }
+//this function is here to print the scene only!
+void Genre::displayScene(Scene* currScene)
+{
+    OutputText *output = new OutputText(); 
+
+    string str = currScene->getText();
+    while(str != ""){
+        output->printingText(str,100);
+        currScene->getText();
+    }
+}
+
