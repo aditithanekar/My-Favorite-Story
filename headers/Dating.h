1d1a8144d84d775574a067ffcadc46fc79e7b5db
aditithanekar <aditipthanekar@gmail.com>
2023-06-03 19:09:20 -0700

    added a getRoot() function to the Dating.h for testing purposes

--- headers/Dating.h
+++ headers/Dating.h
@@ -7,5 +7,15 @@ class Dating:public Genre
         ~Dating();
         void playScene();
         void setupScenes();
+        SceneNode* getRoot()
+        {
+            if(root!=nullptr)
+            {
+                return root;
+            }
+            return nullptr;
+            
+        }
+
 
 };
